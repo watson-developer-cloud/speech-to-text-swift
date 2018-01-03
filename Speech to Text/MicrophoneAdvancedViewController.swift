c3dd167ec7b61ead2749865c5e69e2992e3494d7
Glenn R. Fisher <glennrfisher@gmail.com>
2018-01-03 09:34:37 -0600

    Add MicrophoneAdvancedViewController

---
+++ Speech to Text/MicrophoneAdvancedViewController.swift
@@ -0,0 +1,84 @@
+/**
+ * Copyright IBM Corporation 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+import UIKit
+import SpeechToTextV1
+
+/// This file demonstrates an advanced configuration using the `SpeechToTextSession` class.
+class MicrophoneAdvancedViewController: UIViewController {
+
+    var session: SpeechToTextSession!
+    var settings: RecognitionSettings!
+    var isSessionStarted = false
+    var isStreaming = false
+
+    @IBOutlet weak var sessionButton: UIButton!
+    @IBOutlet weak var microphoneButton: UIButton!
+    @IBOutlet weak var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // use `SpeechToTextSession` for advanced configuration
+        session = SpeechToTextSession(
+            username: Credentials.SpeechToTextUsername,
+            password: Credentials.SpeechToTextPassword
+        )
+
+        // define recognition session callbacks
+        session.onConnect = { print("connected") }
+        session.onDisconnect = { print("disconnected") }
+        session.onError = { error in print(error) }
+        session.onPowerData = { decibels in print(decibels) }
+        session.onMicrophoneData = { data in print("received data") }
+        session.onResults = { results in self.textView.text = results.bestTranscript }
+
+        // define recognition settings
+        settings = RecognitionSettings(contentType: .opus)
+        settings.interimResults = true
+        settings.inactivityTimeout = -1
+    }
+
+    @IBAction func didPressSessionButton(_ sender: UIButton) {
+        if !isSessionStarted {
+            isSessionStarted = true
+            sessionButton.setTitle("Stop Session", for: .normal)
+            session.connect()
+            session.startRequest(settings: settings)
+        } else {
+            isSessionStarted = false
+            isStreaming = false
+            sessionButton.setTitle("Start Sesson", for: .normal)
+            microphoneButton.setTitle("Start Microphone", for: .normal)
+            session.stopMicrophone()
+            session.stopRequest()
+            session.disconnect()
+        }
+    }
+
+    @IBAction func didPressMicrophoneButton(_ sender: UIButton) {
+        if !isStreaming && isSessionStarted {
+            isStreaming = true
+            microphoneButton.setTitle("Stop Microphone", for: .normal)
+            session.startMicrophone()
+        } else {
+            isStreaming = false
+            microphoneButton.setTitle("Start Microphone", for: .normal)
+            session.stopMicrophone()
+        }
+    }
+}
+
